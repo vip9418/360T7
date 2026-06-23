cd4834a4e0a6aa309862b8404b031dbe195d72cf
vip9418 <vip9418@outlook.com>
2026-06-24 07:45:58 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sed -i 's/192.168.6.1/192.168.2.1/' ./package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt-2.4G/NW/' ./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
-sed -i 's/ImmortalWrt-5G/NW/' ./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i 's/ImmortalWrt-2.4G/cmcc/' ./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i 's/ImmortalWrt-5G/cmcc_5G/' ./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 sed -i 's/36/auto/' ./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 sed -i 's/encryption=none/encryption=sae-mixed/' ./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 sed -i '/encryption=sae-mixed/a \ \ \ \ set wireless.default_${dev}.key=12345678' ./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
